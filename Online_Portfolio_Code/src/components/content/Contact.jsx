fa9e4ac57fb8cb563743ac30d03500e7d56862e8
Phoebe L <pholee05@gmail.com>
2025-03-27 22:57:58 +0000

    Contact section

--- Online_Portfolio_Code/src/components/content/Contact.jsx
+++ Online_Portfolio_Code/src/components/content/Contact.jsx
@@ -1,14 +1,37 @@
 export const Contact = () => {
-    return (
-      <section
-        id="contact"
-        className="min-h-screen flex items-center justify-center py-20"
-      >
-        <div className="px-4 w-150">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 bg-clip-text text-transparent to-cyan-400 text-center">
-            Get In Touch
-          </h2>
+  return (
+    <section
+      id="contact"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <div className="max-w-5xl mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 bg-clip-text text-transparent to-cyan-400 text-center">
+          Get In Touch
+        </h2>
+
+        <div className="cursor-pointer flex flex-col space-y-4 items-center">
+          <a
+            href="https://www.linkedin.com/in/pholee"
+            target="_blank"
+            className="text-2xl text-gray-600 hover:text-black transition-colors"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://github.com/pholee"
+            target="_blank"
+            className="text-2xl text-gray-600 hover:text-black transition-colors"
+          >
+            Github
+          </a>
+          <a
+            href="mailto:your-email@example.com"
+            className="text-2xl text-gray-600 hover:text-black transition-colors"
+          >
+            Email
+          </a>
         </div>
-      </section>
-    );
-  };
+      </div>
+    </section>
+  );
+};
--- Online_Portfolio_Code/src/components/content/Home.jsx
+++ Online_Portfolio_Code/src/components/content/Home.jsx
@@ -4,17 +4,13 @@ export const Home = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex items-center justify-left relative"
+      className="min-h-screen flex items-center justify-center relative"
     >
       <RevealOnScroll>
-        <div className="max-w-5xl mx-auto text-left z-10 px-4">
-          {/* Logo */}
-          <img src="/PL-logo.svg" alt="PL-Logo" className="w-60 h-60 mb-4"></img>
-
-          <div>
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 bg-clip-text text-transparent to-cyan-400 leading-right">
+        <div className="text-center z-10 px-4">
+          <hi className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 bg-clip-text text-transparent to-cyan-400 leading-right">
             Hi, I'm Phoebe
-          </h1>
+          </hi>
 
           <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
             I’m Phoebe, a Computer Science student currently studying at City St
@@ -25,7 +21,8 @@ export const Home = () => {
             quick brown fox jumps over hte lazy hare. The quick brown fox jumps
             over the lazy hare. The quick brown fox jumps over the lazy hare.
           </p>
-          </div>
+
+          <div></div>
         </div>
       </RevealOnScroll>
     </section>
